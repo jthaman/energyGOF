7131a9961bb8b71e1014c1f81604f44d9be25a08
John Haman <mail@johnhaman.org>
2025-07-18 20:26:38 -0400

    Table

--- R/energygof.R
+++ R/energygof.R
@@ -812,6 +812,30 @@ StandardCauchyGOFGen <- R6::R6Class(
   )
 )
 
+tabular <- function(df, ...) {
+  stopifnot(is.data.frame(df))
+
+  align <- function(x) if (is.numeric(x)) "r" else "l"
+  col_align <- vapply(df, align, character(1))
+
+  cols <- lapply(df, format, ...)
+  contents <- do.call("paste",
+                      c(cols, list(sep = " \\tab ", collapse = "\\cr\n#'   ")))
+
+  paste("#' \\tabular{", paste(col_align, collapse = ""), "}{\n#'   ",
+        paste0("\\strong{", names(df), "}", sep = "", collapse = " \\tab "), " \\cr\n#'   ",
+        contents, "\n#' }\n", sep = "")
+}
+
+deats <- data.frame(
+  Distribution = character(1),
+  Paramater = character(1),
+  CompositeAllowed = character(1)
+)
+
+deats <- rbind(deats, list("Normal", "mean, sd", "Yes"))
+deats <- rbind(deats, list("Uniform", "min, max", "No"))
+
 ## ##### Cauchy
 ##
 ## ##### Pareto
